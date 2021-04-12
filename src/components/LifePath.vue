b8f54494095c2037a01980d05756d4a405553e04
deadbinky <cubiclemonkee@gmail.com>
2021-04-12 09:42:40 -0600

    lifepath updates

--- src/components/LifePath.vue
+++ src/components/LifePath.vue
@@ -183,11 +183,11 @@
   .spread
     align-items: center
     display: grid
-    grid-template-rows: auto 1fr 1fr
-    grid-template-columns: repeat(3, 1fr)
+    grid-template-rows: auto 1fr 1fr auto
+    grid-template-columns: repeat(2, 1fr)
     justify-content: center
     margin: auto
-    padding-top: 0
+    padding-top: $bodyPaddingTop
     max-width: 500px
     position: relative
     vertical-align: middle
@@ -204,27 +204,31 @@
         padding-top: 166%
 
       &:nth-of-type(2)
-        grid-column: 2
+        grid-column: 1/3
         grid-row: 2
+        width: 47.5%
 
       &:nth-of-type(3)
         grid-column: 1
         grid-row: 3
 
       &:nth-of-type(4)
-        grid-column: 3
+        grid-column: 2
         grid-row: 3
 
       &:nth-of-type(5)
-        grid-column: 2
+        grid-column: 1/3
         grid-row: 3
+        width: 47.5%
+
 </style>
 <style lang='sass'>
   @import '../assets/sass/_colours'
 
   .vdp-datepicker
-    grid-column: 2
+    grid-column: 1/3
     grid-row: 1
+    margin: auto
 
     .vdp-datepicker__calendar .cell.selected
       background: $mediumpink
